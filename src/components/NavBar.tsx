f6e33cc4616e3fdfa1220544d9e7587c35627b5f
Akaikenlol <keithken972@gmail.com>
2024-03-29 20:44:21 +0630

    feat: Add mobile navigation menu to NavBar component.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,110 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import WordMark from "./WordMark";
-import { Content } from "@prismicio/client";
+import { Content, asLink } from "@prismicio/client";
 import Link from "next/link";
 import { PrismicNextLink } from "@prismicio/next";
 import ButtonLink from "./ButtonLink";
+import { MdClose, MdMenu } from "react-icons/md";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 type NavBarProp = {
   settings: Content.SettingsDocument;
 };
 
 const NavBar = ({ settings }: NavBarProp) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
   return (
     <nav aria-label="Main" className="px-4 py-4 md:px-6 md:py-6">
       <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
-        <Link href={"/"}>
-          <WordMark />
-          <span className="sr-only">Glisten.ai Home Page</span>
-        </Link>
-        <ul className="flex gap-6">
+        <div className="flex items-center justify-between">
+          <Link href={"/"} className="z-50" onClick={() => setOpen(false)}>
+            <WordMark />
+            <span className="sr-only">Glisten.ai Home Page</span>
+          </Link>
+          <button
+            type="button"
+            className="block p-2 text-3xl text-white md:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(true)}
+          >
+            <MdMenu />
+            <span className="sr-only">Open Menu</span>
+          </button>
+        </div>
+        {/* Mobile Nav */}
+        <div
+          className={clsx(
+            "fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-end gap-4 bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transition-none md:hidden",
+            open ? "translate-x-0" : "translate-x-[100%]",
+          )}
+        >
+          <button
+            type="button"
+            className="fixed right-4 top-4 mb-4 block p-2 text-3xl text-white md:hidden"
+            aria-expanded={open}
+            onClick={() => setOpen(false)}
+          >
+            <MdClose />
+            <span className="sr-only">Close Menu</span>
+          </button>
+
+          <div className="grid justify-items-end gap-8">
+            {settings.data.navigation.map((item) => {
+              if (item.cta_button) {
+                return (
+                  <ButtonLink
+                    key={item.label}
+                    field={item.link}
+                    onClick={() => setOpen(false)}
+                    aria-current={
+                      pathname.includes(asLink(item.link) as string)
+                        ? "page"
+                        : undefined
+                    }
+                  >
+                    {item.label}
+                  </ButtonLink>
+                );
+              }
+              return (
+                <PrismicNextLink
+                  key={item.label}
+                  field={item.link}
+                  className="block px-3 text-3xl first:mt-8"
+                  onClick={() => setOpen(false)}
+                  aria-current={
+                    pathname.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
+                >
+                  {item.label}
+                </PrismicNextLink>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* Desktop Nav */}
+        <ul className="hidden gap-6 md:flex">
           {settings.data.navigation.map((item) => {
             if (item.cta_button) {
               return (
                 <li key={item.label}>
-                  <ButtonLink field={item.link}>{item.label}</ButtonLink>
+                  <ButtonLink
+                    field={item.link}
+                    aria-current={
+                      pathname.includes(asLink(item.link) as string)
+                        ? "page"
+                        : undefined
+                    }
+                  >
+                    {item.label}
+                  </ButtonLink>
                 </li>
               );
             }
@@ -33,6 +113,11 @@ const NavBar = ({ settings }: NavBarProp) => {
                 <PrismicNextLink
                   field={item.link}
                   className="inline-flex min-h-11 items-center"
+                  aria-current={
+                    pathname.includes(asLink(item.link) as string)
+                      ? "page"
+                      : undefined
+                  }
                 >
                   {item.label}
                 </PrismicNextLink>
